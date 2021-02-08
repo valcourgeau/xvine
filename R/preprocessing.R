dddb6f518d50e573e1739e1bfe850f8d24be7b89
Valentin Courgeau <valentin.courgeau@free.fr>
2021-02-08 12:43:30 +0000

    add integral transform

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,9 +1,80 @@
+# fits and applies ecdf on each column
 uniform_ecdf <- function(x){
+  ecdf_handle <-  function(x_col){ecdf(x_col)(x_col)}
   if(is.matrix(x)){
     return(apply(
         x, MARGIN = 2,
-        FUN = function(x_col){ecdf(x_col)(x_col)}
+        FUN = ecdf_handle
       )
     )
+  }else{
+    if(is.list(x)){
+      return(lapply(
+          x, FUN = ecdf_handle
+        )
+      )
+    }
   }
+}
+
+#apply ecdf on data below quantile u0
+partial_ecdf <- function(x, u0){
+  # u0 is a quantile value between 0 and 1
+  # x is a vector
+  y <- x[x <= quantile(x, u0)]
+  return(list('data'=ecdf(y)(y)))
+}
+
+# GPD MLE for data above quantile u0
+partial_gpd <- function(x, u0){
+  # u0 is a quantile value between 0 and 1
+  # x is a vector
+  thres <- quantile(x, u0)
+  gpd_fit <- evir::gpd(data = x, threshold = thres)
+  return(
+    list(
+      'data'=evir::pgpd(gpd_fit$data-thres, xi = gpd_fit$par.ests[1], beta = gpd_fit$par.ests[2]),
+      'par.ests'=gpd_fit$par.ests,
+      'par.ses'=gpd_fit$par.ses
+    )
+  )
+}
+
+# Integral transform
+integral_transform <- function(x, u0){
+  # u0 is a quantile value between 0 and 1
+  # x is a vector
+  thres <- quantile(x, u0)
+  p_ecdf <- partial_ecdf(x, u0)
+  p_gpd <- partial_gpd(x, u0)
+  x[x <= thres] <- p_ecdf[['data']] * u0
+  x[x > thres] <- p_gpd[['data']] * (1. - u0) + u0
+  return(
+    list(
+      'data'=x,
+      'par.ests'=p_gpd$par.ests,
+      'par.ses'=p_gpd$par.ses
+    )
+  )
+}
+
+# Applying integral transform on each marginal
+apply_integral_transform <- function(data, u0s){
+  stopifnot(ncol(data) == length(u0s))
+  # apply it on each marginal
+  data_it <- lapply(
+    1:length(u0s),
+    function(i){integral_transform(data[,i], u0 = u0s[i])}
+  )
+
+  data_it_values <- do.call(cbind, lapply(data_it, function(x){x$data}))
+  data_it_ests <- do.call(rbind, lapply(data_it, function(x){x$par.ests}))
+  data_it_ses <- do.call(rbind, lapply(data_it, function(x){x$par.ses}))
+  return(
+    list(
+      'data'=data_it_values,
+      'par.ests'=data_it_ests,
+      'par.ses'=data_it_ses
+    )
+  )
 }
--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -5,3 +5,59 @@ test_that("preprocessing/uniform_ecdf/", {
   x_ecdf <- uniform_ecdf(x)
   testthat::expect_equal(dim(x_ecdf), c(n/n_col, n_col))
 })
+
+test_that("preprocessing/partial_ecdf/", {
+  n <- 1000
+  quant <- .75
+  x <- runif(n = n, min = 0, max = 1)
+
+  x_partial_ecdf <- partial_ecdf(x, .75)[['data']]
+
+  testthat::expect_equal(length(x_partial_ecdf), n * quant)
+  testthat::expect_lte(max(x_partial_ecdf), 1.0)
+  testthat::expect_gte(max(x_partial_ecdf), 0.0)
+})
+
+test_that("preprocessing/partial_gpd/", {
+  n <- 1000
+  quant <- .75
+  xi <- 0.1
+  beta <- 1.
+  x <- evir::rgpd(n = n, xi = xi, beta = beta)
+
+  x_partial_ecdf <- partial_gpd(x, .75)[['data']]
+
+  testthat::expect_equal(length(x_partial_ecdf), n * (1-quant))
+  testthat::expect_lte(max(x_partial_ecdf), 1.0)
+  testthat::expect_gte(max(x_partial_ecdf), 0.0)
+})
+
+test_that("preprocessing/integral_transform/", {
+  n <- 10000
+  quant <- .75
+  xi <- 0.1
+  beta <- 1.
+  x <- evir::rgpd(n = n, xi = xi, beta = beta)
+
+  x_it <- integral_transform(x, u0=quant)[['data']]
+  hist(x_it, breaks=50)
+  testthat::expect_equal(length(x_it), n)
+  testthat::expect_lte(max(x_it), 1.0)
+  testthat::expect_gte(max(x_it), 0.0)
+  testthat::expect_gte(ks.test(x_it, "punif", 0, 1)$p.value, .05)
+})
+
+test_that("preprocessing/apply_integral_transform/", {
+  n <- 1000
+  ncol <- 10
+  quant <- .75
+  xi <- 0.1
+  beta <- 1.
+  x <- matrix(evir::rgpd(n = n*ncol, xi = xi, beta = beta), ncol = ncol, nrow = n)
+
+  u0s <- rep(quant, ncol)
+  x_it <- apply_integral_transform(x, u0s=u0s)
+  testthat::expect_equal(dim(x_it$data), c(n, ncol))
+  testthat::expect_lte(max(x_it$data), 1.0)
+  testthat::expect_gte(max(x_it$data), 0.0)
+})
